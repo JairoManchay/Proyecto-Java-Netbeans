57a69a6009b48db79c23c7ba5d211cb067269c0a
YoshikazuCasanova <70545382+Yoshikazu27@users.noreply.github.com>
2025-05-29 14:35:27 -0500

    Actualizacion BD
    Eliminacion de tablas Categoria, FormaPago y derivados de Documento. Creacion de nuevos campos.

--- nbproject/bd/Script - Creacion Tablas.sql
+++ nbproject/bd/Script - Creacion Tablas.sql
@@ -1,15 +1,10 @@
 use bd_restaurante;
 
-create table categoria (
-	codigo char(4) not null,
-    nombre varchar(30) not null
-);
-
 create table producto (
 	codigo char(6) not null,
     descripcion varchar(50) not null,
     precio numeric(5,2) not null,
-    id_categoria char(4) not null
+    categoria varchar(30) not null
 );
 
 create table detalle_documento (
@@ -22,29 +17,15 @@ create table detalle_documento (
 create table documento (
 	codigo char(12) not null,
     fecha datetime not null,
-    id_forma_pago char(3) not null,
+    tipo varchar(20) not null,
+    num_cliente varchar(11) null,
+    nombre_cliente varchar(60) null,
+    forma_pago varchar(20) not null,
     num_mesa tinyint
-);
-
-create table forma_pago (
-	codigo char(3) not null,
-    nombre varchar(20) not null
 );
 
 create table mesa (
 	numero tinyint not null,
     asientos tinyint,
     disponible bit not null default 1
-);
-
-create table boleta (
-	id_documento char(12) not null,
-    dni char(8) not null,
-    nombres varchar(60) not null
-);
-
-create table factura (
-	id_documento char(12) not null,
-    ruc char(11) not null,
-    razon_social varchar(60) not null
 );
