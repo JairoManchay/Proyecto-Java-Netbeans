e5355eec68ff23591d05b62d848a1ea61a70c7c0
JairoManchay <jairorodrigomanchayperez@gmail.com>
2025-06-04 22:02:16 -0500

    Creando las clases principales - Jairo Manchay

---
+++ src/com/org/restaurante/dto/Order.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.org.restaurante.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Order {
+    private String code;
+    private LocalDate createAt;
+    private Bill bill;
+    private Table table;
+    private List<OrderDetail> listDetail;
+
+    
+    
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
+    public Bill getBill() {
+        return bill;
+    }
+
+    public void setBill(Bill bill) {
+        this.bill = bill;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    public List<OrderDetail> getListDetail() {
+        return listDetail;
+    }
+
+    public void setListDetail(List<OrderDetail> listDetail) {
+        this.listDetail = listDetail;
+    }
+    
+    
+}
